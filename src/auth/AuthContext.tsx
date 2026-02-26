49590c56ff03d7c003aaa11e30f5031c7bf946c7
Santiago Prieto <spriet0234@gmail.com>
2026-02-25 22:38:06 -0600

    feat login component addition

--- src/auth/AuthContext.tsx
+++ src/auth/AuthContext.tsx
@@ -1,25 +1,43 @@
-import { type ReactNode } from 'react'
-import { createContext } from 'react'
+import { useEffect, type ReactNode, createContext, useState } from 'react'
 
 
 interface AuthContextType {
         isAuthenticated: boolean
+        token: string|null
+        login: (token:string) => void
+        logout: () => void
     }
 
 type AuthProviderProps = {
   children: ReactNode
 }
+
+
+
 export const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export const AuthProvider = ({children}: AuthProviderProps) => {
 
+  const [token,setToken] = useState<string | null>(() => localStorage.getItem("token"))
+  
+  const isAuthenticated = !!token
 
-    
+  const login = (newToken: string) => {
+  setToken(newToken)
+  localStorage.setItem("token", newToken)
+}
+  const logout = () =>{
+  setToken(null)
+  localStorage.removeItem("token")
+}
 
   return (
-    <AuthContext.Provider value = {{isAuthenticated: false}}>
+
+   <AuthContext.Provider value = {{isAuthenticated: isAuthenticated, token, login, logout}}>
       {children}
-    </AuthContext.Provider>
+    </AuthContext.Provider> 
+    
+    
   )
 }
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import { useState, useContext } from "react";
+import { AuthContext } from "../auth/AuthContext";
 
-const LoginPage: React.FC = () => {
+const LoginPage = () => {
+    const [token,setToken] = useState<string | null>()
+    const [email,setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const {login} = useContext(AuthContext)
+
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        login(email,password);
+    }
+   
+
+
+        
+      
     return (
         <div>
             <h1>Login</h1>
             <p>Welcome to the login page</p>
+            <form onSubmit={handleSubmit}>
+            <input type="text" name = "email" 
+            onChange = {(e)=> setEmail(e.target.value)} 
+            ></input>
+
+            <input type="text" name = "password" 
+            onChange = {(e)=> {setPassword(e.target.value)
+            }} 
+            ></input>
+            </form>
+
         </div>
+
     );
 };
 
